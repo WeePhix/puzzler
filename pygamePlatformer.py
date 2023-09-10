d675e9619a4f065a73be7bf5387ba13e3459d1c2
WeePhix <evanjasaromih@gmail.com>
2023-09-11 00:49:56 +0200

    Redid most of the project TODO: fix errors ;-;

--- pygamePlatformer.py
+++ pygamePlatformer.py
@@ -4,149 +4,170 @@ from sys import exit
 
 tileSize = 80
 tilesX = tilesY = 9
+global moveDir
+moveDir = [0, 0]
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('puzzler/graphics/player.png').convert_alpha()
+        self.image = pygame.image.load('graphics/player.png').convert_alpha()
         self.image = pygame.transform.scale(self.image, (tileSize, tileSize))
         self.rect = self.image.get_rect()
+        self.rect.center = (screen.get_width() / 2, screen.get_height() / 2)
         
-        self.diff = [(-tileSize, -tileSize), (0, -tileSize), (tileSize, -tileSize), (tileSize, 0), (tileSize, tileSize), (0, tileSize), (-tileSize, tileSize), (-tileSize, 0)]
-        self.moveDir = [0, 0]
         self.moving = False
+        self.surrWalls = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        self.moveFreedom = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        self.animCounter = 0
+        self.pixelCounter = 0
         
-    def eventHandler(self, events):
-        print(f'player.eventHandler: moveDir = {self.moveDir}')
-        self.moveDir = list(self.moveDir)
-        for event in events:
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_w:
-                    self.moveDir[1] -= 1
-                if event.key == pygame.K_s:
-                    self.moveDir[1] += 1
-                if event.key == pygame.K_a:
-                    self.moveDir[0] -= 1
-                if event.key == pygame.K_d:
-                    self.moveDir[0] += 1
-    
-    def checkMovementOptions(self):
-        print(f'player.checkMovementOptions: moveDir = {self.moveDir}')
+    def checkMoveOptions(self, walls):
+        self.surrWalls = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        moveFreedom = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
         
-        i = 0
-        surr = ['empty'] * 8
-        for tile in pygame.sprite.Group.sprites(tileGroup):
-            if (tile.rect.center[0] - self.rect.center[0], tile.rect.center[1] - self.rect.center[1]) == self.diff[i]:
-                surr.append(tile)
-                i += 1
+        for wall in pygame.sprite.Group.sprites(walls):
+            if wall.rect.centery - self.rect.centery == -tileSize:
+                if wall.rect.centerx - self.rect.centerx == -tileSize:
+                    self.surrWalls[0][0] = wall
+                elif wall.rect.centerx - self.rect.centerx == 0:
+                    self.surrWalls[0][1] = wall
+                elif wall.rect.centerx - self.rect.centerx == tileSize:
+                    self.surrWalls[0][2] = wall
+            elif wall.rect.centery - self.rect.centery == 0:
+                if wall.rect.centerx - self.rect.centerx == -tileSize:
+                    self.surrWalls[1][0] = wall
+                elif wall.rect.centerx - self.rect.centerx == 0:
+                    self.surrWalls[1][1] = wall
+                elif wall.rect.centerx - self.rect.centerx == tileSize:
+                    self.surrWalls[1][2] = wall
+            elif wall.rect.centery - self.rect.centery == tileSize:
+                if wall.rect.centerx - self.rect.centerx == -tileSize:
+                    self.surrWalls[2][0] = wall
+                elif wall.rect.centerx - self.rect.centerx == 0:
+                    self.surrWalls[2][1] = wall
+                elif wall.rect.centerx - self.rect.centerx == tileSize:
+                    self.surrWalls[2][2] = wall
         
-        if surr == ['empty'] * 8:
-            return 0
+        for y in range(3):
+            for x in range(3):
+                if x != 1 and y != 1:
+                    self.moveFreedom[x][y] = not(self.surrWalls[y][1] or self.surrWalls[1][x] or self.surrWalls[x][y])
+                elif self.surrWalls[y][x] == 0:
+                    self.moveFreedom[y][x] = 1
         
-        if (surr[1] == 'full' and self.moveDir[1] == -1) or (surr[5] == 'full' and self.moveDir[1] == 1):
-            self.moveDir[1] = 0
-            print('blocked movement vertically')
-        if (surr[3] == 'full' and self.moveDir[0] == 1) or (surr[7] == 'full' and self.moveDir[0] == -1):
-            self.moveDir[0] = 0
-            print('blocked movement horizontally')
-            
-        if (surr[1] == 'pathNS' and self.moveDir == [0, -1]) or (surr[5] == 'pathNS' and self.moveDir == [0, 1]):
-            self.moveDir[1] *= 2
-        elif (surr[3] == 'pathWE' and self.moveDir == [1, 0]) or (surr[7] == 'pathWE' and self.moveDir == [-1, 0]):
-            self.moveDir[0] *= 2
-        # ADD CORNER PATH CHECKS!!!!
-        else:
-            self.moveDir = (0, 0)
-            print('blocked movement')
+    def defineMoveDir(self):
+        moveVec = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        moveVec[moveDir[1] + 1][moveDir[0] + 1] = 1
         
+        moveVec[0][1] = moveVec[0][0] or moveVec[0][1] or moveVec[0][2]
+        moveVec[2][1] = moveVec[2][0] or moveVec[2][1] or moveVec[2][2]
+        moveVec[1][0] = moveVec[0][0] or moveVec[1][0] or moveVec[2][0]
+        moveVec[1][2] = moveVec[0][2] or moveVec[1][2] or moveVec[2][2]
+        
+        for x in [0, 2, 1]:
+            if self.moving: break
+            for y in [0, 2, 1]:
+                if moveVec[y][x] and self.moveFreedom[y][x]:
+                    self.direction = (x-1, y-1)
+                    self.moving = True
+                    break
+
     def move(self):
-        print(f'player.move: moveDir = {self.moveDir}')
-        self.moving = True
-        for _ in range(max(self.moveDir) * 16):            
-            screen.fill('black')
-            tileGroup.draw(screen)
-            player.draw(screen)
-            pygame.time.delay(1000//16)
-        self.moveDir = [0, 0]
-        self.moving = False
+        self.animCounter += 1
+        self.pixelCounter += 1
+        if self.animCounter >= 2:
+            self.animCounter = 0
+            self.rect.x += self.direction * tileSize / 16
+        
+        if self.pixelCounter == 16:
+            self.moving = False
     
-    def update(self, events):
-        if self.moving:
-            return 0
-        
-        self.eventHandler(events)
-        self.checkMovementOptions()
-        self.move()
-        
+    def update(self):
+        self.checkMoveOptions(pygame.sprite.Group.sprites(wallGroup))
+        self.defineMoveDir()
+        if self.moving: self.move()
 
-
-
-class Tile(pygame.sprite.Sprite):
-    def __init__(self, type, coords):
+class Floor(pygame.sprite.Sprite):
+    def __init__(self, coords):
         super().__init__
         pygame.sprite.Sprite.__init__(self)
         
-        self.image = pygame.image.load('puzzler/graphics/' + type + '.png').convert_alpha()
+        self.image = pygame.image.load('graphics/floor.png').convert_alpha()
         self.image = pygame.transform.scale(self.image, (tileSize, tileSize))
         
         self.rect = self.image.get_rect()
         self.rect.topleft = coords[0] * tileSize, coords[1] * tileSize
         
-        self.type = type
+        self.coords = coords
 
 
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, coords):
+        super().__init__
+        pygame.sprite.Sprite.__init__(self)
+        
+        self.image = pygame.image.load('graphics/wall.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (tileSize, tileSize))
+        
+        self.rect = self.image.get_rect()
+        self.rect.topleft = coords[0] * tileSize, coords[1] * tileSize
+        
+        self.coords = coords
+
+    
+# HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+
+def levelWallsAndFloor(array):
+    for y, row in enumerate(array):
+        for x, tile in enumerate(row):
+            if tile == 1:
+                wallGroup.add(Wall((x, y)))
+            elif tile == 0:
+                floorGroup.add(Floor((x, y)))
 
 
-def levelToTileTypes(level):
-    for y in range(tilesY):
-        for x in range(tilesX):
-            if type(level[y][x]) == int:
-                if level[y][x] >= len(tileTypes):
-                    return 'ERROR: incorrect tile index!'
-                level[y][x] = tileTypes[level[y][x]]
-    return level
-
-
-def tileArrayToGroup(array, group):
-    for y in range(len(array)):
-        for x in range(len(array[y])):
-            if array[y][x] != 'none':
-                group.add(Tile(array[y][x], (x, y)))
-
+level = [[1] * 9] + [[1, 0, 0, 0, 0, 0, 0, 0, 1]] * 7 + [[1] * 9]
 
 pygame.init()
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((tileSize * tilesX, tileSize * tilesY))
 
-# explanation:         |X|       |\        |/        \|        /|        ||        =
-tileTypes = ['none', 'full', 'pathNE', 'pathSE', 'pathSW', 'pathNW', 'pathNS', 'pathWE']
-
-level = [[1] * 9] + [[1, 0, 0, 0, 0, 0, 0, 0, 1]] * 7 + [[1] * 9]
-
-level = levelToTileTypes(level)
-
-tileGroup = pygame.sprite.Group()
-
+wallGroup = pygame.sprite.Group()
+floorGroup = pygame.sprite.Group()
 player = pygame.sprite.GroupSingle(Player())
-player.sprite.rect.center = (tilesX * tileSize / 2, tilesY * tileSize / 2)
 
-tileArrayToGroup(level, tileGroup)
+levelWallsAndFloor(level)
 
 while True:
-    screen.fill('black')
-    tileGroup.draw(screen)
-    player.draw(screen)
-    
-    events = []
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        else:
-            events.append(event)
-    player.update(events)
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                moveDir[1] -= 1
+            elif event.key == pygame.K_s:
+                moveDir[1] += 1
+            elif event.key == pygame.K_a:
+                moveDir[0] -= 1
+            elif event.key == pygame.K_d:
+                moveDir[0] += 1
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_w:
+                moveDir[1] += 1
+            elif event.key == pygame.K_s:
+                moveDir[1] -= 1
+            elif event.key == pygame.K_a:
+                moveDir[0] += 1
+            elif event.key == pygame.K_d:
+                moveDir[0] -= 1
+    
+    player.update()
+    wallGroup.draw(screen)
+    floorGroup.draw(screen)
+    player.draw(screen)
     
     pygame.display.update()
-    clock.tick(32)
+    clock.tick(64)
